6517252e783f7c7275531d46d7439bb693c3b1fa
Snakeshin0101 <edgardocazo@hotmail.com>
2022-11-25 12:00:09 -0600

    v-1.0

---
+++ portscanner.py
@@ -0,0 +1,28 @@
+import socket
+import termcolor
+
+def scan(target, ports):
+    for port in range(1, ports):
+        scan_port(target, port)
+
+def scan_port(ipaddress, port):
+    try:
+        sock = socket.socket()
+        sock.connect((ipaddress, port))
+        print('[+] Port Open ' + str(port))
+        sock.close()
+
+    except:
+        print('[-] Port Close ' + str(port))
+
+targets = input('[*] Enter targets to scan (split them by ,): ')
+ports = input('[*] Enter how many ports you want to scan: ')
+
+if ',' in targets:
+    print('[*] Scanning Multiple Targets...')
+    for ip_addr in targets.split(','):
+        scan(ip_addr.strip(''), ports)
+else:
+    scan(targets, ports)
+
+scan(targets, ports)
